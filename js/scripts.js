2f5c7e8aca5707ee8f402c7fc144b22f8f10f3f7
huangzwa <huangzwa@yonyou.com>
2015-06-25 14:58:02 +0800

    Site updated: 2015-06-25 14:57:58

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,78 @@
 $(document).ready(function($) {
-    "use strict";
-    var lightnav = $(".lightnav .navbar-inner"),
-    	darknav = $(".darknav .navbar-inner");
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 10) {
-            lightnav.addClass("lightnav-alt");
-            darknav.addClass("darknav-alt");
-        } else {
-            lightnav.removeClass("lightnav-alt");
-            darknav.removeClass("darknav-alt");
-        }
-    });
+	"use strict";
+	var lightnav = $(".lightnav .navbar-inner"),
+		windowH = window.innerHeight - 60,
+		darknav = $(".darknav .navbar-inner");
+	$(window).scroll(function() {
+		var s = $(this).scrollTop();
+		if (s > windowH) {
+			lightnav.addClass("lightnav-alt");
+			//darknav.addClass("darknav-alt");
+		} else {
+			lightnav.removeClass("lightnav-alt");
+			//darknav.removeClass("darknav-alt");
+		}
+	});
 
 	$(document).ready(function($) {
-
 		// Slidebars off-canvas menu
 		$.slidebars();
-
 		// Popovers [Hover]
 		$("[data-toggle=popover]")
 			.popover({
-				html:true
-			}
-		);
-		
+				html: true
+			});
+
 		$("html").niceScroll({
 			smoothscroll: true, // scroll with ease movement
 			autohidemode: false,
 			zindex: "100", // change z-index for scrollbar div
-        	scrollspeed: 60, // scrolling speed
-        	mousescrollstep: 40,
-        	gesturezoom: false,
-        	horizrailenabled: false,
-        	cursorcolor: "#151515",
-	        boxzoom: false,
-	        cursorborder: "0 solid #202020",
-	        cursorborderradius: "5px",
-	        cursorwidth: 9,
-	        enablemousewheel: true,
-	        background: "rgba(255,255,255,0.7)",
+			scrollspeed: 60, // scrolling speed
+			mousescrollstep: 40,
+			gesturezoom: false,
+			horizrailenabled: false,
+			cursorcolor: "#151515",
+			boxzoom: false,
+			cursorborder: "0 solid #202020",
+			cursorborderradius: "5px",
+			cursorwidth: 9,
+			enablemousewheel: true,
+			background: "rgba(255,255,255,0.7)",
 		});
 
 		// Page transitions
 		$(".animsition").animsition({
 
-			inClass               :   'fade-in',
-			outClass              :   'fade-out-down-sm',
-			inDuration            :    900,
-			outDuration           :    800,
-			linkElement           :   '.animsition-link', 
+			inClass: 'fade-in',
+			outClass: 'fade-out-down-sm',
+			inDuration: 900,
+			outDuration: 800,
+			linkElement: '.animsition-link',
 			//e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-			loading               :    true,
-			loadingParentElement  :   'body', //animsition wrapper element
-			loadingClass          :   'animsition-loading',
-			unSupportCss          : [ 'animation-duration',
-			                          '-webkit-animation-duration',
-			                          '-o-animation-duration'
-			                        ],
+			loading: true,
+			loadingParentElement: 'body', //animsition wrapper element
+			loadingClass: 'animsition-loading',
+			unSupportCss: ['animation-duration',
+				'-webkit-animation-duration',
+				'-o-animation-duration'
+			],
 			//"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser. 
 			//The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
 
-			overlay               :   false,
+			overlay: false,
 
-			overlayClass          :   'animsition-overlay-slide',
-			overlayParentElement  :   'body'
+			overlayClass: 'animsition-overlay-slide',
+			overlayParentElement: 'body'
 		});
 
 		// WOW plugin settings
-        var wow = new WOW(
-          { animateClass: 'animated', // set our global css classT (default is animated)
-            offset: 250, // set distance to content until it triggers (default is 0)
-            mobile: false, // remove animations for mobiles/tablets (default is true)
-            live: true }); // act on asynchronously loaded content (default is true)
-        new WOW().init();
+		var wow = new WOW({
+			animateClass: 'animated', // set our global css classT (default is animated)
+			offset: 250, // set distance to content until it triggers (default is 0)
+			mobile: false, // remove animations for mobiles/tablets (default is true)
+			live: true
+		}); // act on asynchronously loaded content (default is true)
+		new WOW().init();
 
 		// Functionailty constraints for mobile
 		/*if (!Modernizr.touch) {
@@ -121,14 +121,14 @@ $(document).ready(function($) {
 		});
 
 		// faq's floating sidebar (left)
-	    $('#sidebar').affix({
-	    	offset: {
-	    		top: 500
-	    	}
+		$('#sidebar').affix({
+			offset: {
+				top: 500
+			}
 		});
-		
-	    // Scrollspy for scrollto links in floating faq sidebar
-		var $body   = $(document.body);
+
+		// Scrollspy for scrollto links in floating faq sidebar
+		var $body = $(document.body);
 		var navHeight = $('.navbar').outerHeight(true) + 80;
 
 		$body.scrollspy({
@@ -137,39 +137,39 @@ $(document).ready(function($) {
 		});
 
 		// fade out map cover (contact.html)
-	    $(".map-cover").click(function () {
-	        $(".map-cover").fadeOut("slow");
-	    });
+		$(".map-cover").click(function() {
+			$(".map-cover").fadeOut("slow");
+		});
 
 		// Collapsible panels for faq's and careers
-	    $('.collapse').on('show.bs.collapse', function() {
-	        var id = $(this).attr('id');
-	        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-panel');
-	        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-up"></i>');
-	    });
-	    $('.collapse').on('hide.bs.collapse', function() {
-	        var id = $(this).attr('id');
-	        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-panel');
-	        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-down"></i>');
-	    });
+		$('.collapse').on('show.bs.collapse', function() {
+			var id = $(this).attr('id');
+			$('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-panel');
+			$('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-up"></i>');
+		});
+		$('.collapse').on('hide.bs.collapse', function() {
+			var id = $(this).attr('id');
+			$('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-panel');
+			$('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-chevron-down"></i>');
+		});
 
-	    /*!
-	     * IE10 viewport hack for Surface/desktop Windows 8 bug
-	     * Copyright 2014 Twitter, Inc.
-	     * Licensed under the Creative Commons Attribution 3.0 Unported License. For
-	     * details, see http://creativecommons.org/licenses/by/3.0/.
-	     */
-	    // See the Getting Started docs for more information:
-	    // http://getbootstrap.com/getting-started/#support-ie10-width
-	    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-	        var msViewportStyle = document.createElement('style');
-	        msViewportStyle.appendChild(
-	            document.createTextNode(
-	                '@-ms-viewport{width:auto!important}'
-	            )
-	        );
-	        document.querySelector('head').appendChild(msViewportStyle);
-	    }
+		/*!
+		 * IE10 viewport hack for Surface/desktop Windows 8 bug
+		 * Copyright 2014 Twitter, Inc.
+		 * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+		 * details, see http://creativecommons.org/licenses/by/3.0/.
+		 */
+		// See the Getting Started docs for more information:
+		// http://getbootstrap.com/getting-started/#support-ie10-width
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement('style');
+			msViewportStyle.appendChild(
+				document.createTextNode(
+					'@-ms-viewport{width:auto!important}'
+				)
+			);
+			document.querySelector('head').appendChild(msViewportStyle);
+		}
 
 	}); // Document Ready
 
@@ -183,9 +183,9 @@ $(document).ready(function($) {
 			$(this).add($submenu).toggleClass('sb-submenu-active'); // Toggle active class.
 
 			if ($submenu.hasClass('sb-submenu-active')) {
-			$submenu.slideDown(200);
+				$submenu.slideDown(200);
 			} else {
-			$submenu.slideUp(200);
-		}
-	});
+				$submenu.slideUp(200);
+			}
+		});
 });
